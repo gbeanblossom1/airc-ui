d02b5df529282fcc99ffa1e4e5cfebe388624ae9
Marshk4 <kobie.marsh@vt-arc.org>
2022-08-30 17:29:12 -0400

    Added new graphs to homepage

--- chart.js
+++ chart.js
@@ -1,105 +1,159 @@
-var ctx1 = document.getElementById('barChart').getContext('2d');
-var barChart = new Chart(ctx1, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data : [19, 12, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            dorderwidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-//line
-var ctxL = document.getElementById("lineChart").getContext('2d');
-var myLineChart = new Chart(ctxL, {
-  type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "My First dataset",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    },
-    {
-      label: "My Second dataset",
-      data: [28, 48, 40, 19, 86, 27, 90],
-      backgroundColor: [
-        'rgba(0, 137, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(0, 10, 130, .7)',
-      ],
-      borderWidth: 2
-    }
-    ]
-  },
-  options: {
-    responsive: true
+var queryData;
+const csvData = Papa.parse("data/981_2022_08_30.csv", {
+  download: true,
+  header: true,
+  dynamicTyping: true,
+  complete: function(results) {
+    console.log(results)
+    queryData = results.data    
+    
+    var allData = {};
+    Object.entries(queryData).forEach(([key, value]) => {
+      allData[value['contracting_agency_name']] = value['sum']/1000000000;
+      
+    })
+    var items = Object.keys(allData).map(function(key) {
+      return [key, allData[key]]
+    })
+
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    })
+    console.log(items.slice(0, 5))
+    items = items.slice(0, 5);
+    var depts = [];
+    var dollars = [];
+    $.each(items, function(key, value) {
+      depts.push(value[0]);
+      dollars.push(value[1]);
+      
+    })
+    console.log(depts)
+    console.log(dollars)
+    main(depts, dollars);
   }
 });
-//radar
-var ctxR = document.getElementById("radarChart").getContext('2d');
-var myRadarChart = new Chart(ctxR, {
-  type: 'radar',
-  data: {
-    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-    datasets: [{
-      label: "My First dataset",
-      data: [65, 59, 90, 81, 56, 55, 40],
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    },
-    {
-      label: "My Second dataset",
-      data: [28, 48, 40, 19, 96, 27, 100],
-      backgroundColor: [
-        'rgba(0, 250, 220, .2)',
-      ],
-      borderColor: [
-        'rgba(0, 213, 132, .7)',
-      ],
-      borderWidth: 2
-    }
-    ]
-  },
-  options: {
-    responsive: true
+
+function main(depts, dollars)  {
+  var ctx1 = document.getElementById('barChart').getContext('2d');
+  //TODO turn this into a treemap
+  const barChart = new Chart(ctx1, {
+      type: 'bar',
+      data: {
+          // once this is a treemap then use the depts array instead
+          labels: ['Army', 'Navy', 'Air Force', 'DLA', 'DHA'],
+          datasets: [{
+              label: 'Total Contract Dollars By Agency since 2016, in Billions',
+              data : dollars,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)'
+              ],
+              borderwidth: 1
+          }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      },
+      // plugins: [{
+      //   beforeInit: function (chart) {
+      //     chart.data.labels.forEach(function (value, index, array) {
+      //       var a = [];
+      //       a.push(value.slice(0,5));
+      //       var i = 1;
+      //       while(value.length > (i * 6)) {
+      //         a.push(value.slice(i * 6, (i+1) * 6));
+      //         i++;
+      //       }
+      //       array[index] = a;
+      //     })
+      //   }
+      // }]
+  });
+}
+$(function runNonFileData() {
+  //Second bar chart
+  var ctx2 = document.getElementById('twoChart').getContext('2d');
+
+  const barChart2 = new Chart(ctx2, {
+      type: 'bar',
+      data: {
+          labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+          datasets: [{
+              label: "Total Contract Dollars by Year, in Billions",
+              data : [449, 345, 494, 755, 451, 798, 101],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(21, 212, 125, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(21, 212, 125, 0.2)'
+              ],
+              borderwidth: 1
+          }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+  });
+  google.charts.load('current', {'packages':['gauge']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Label', 'Value'],
+      ['Contracts', 50]
+    ]);
+
+    var options = {
+      width: 400, height: 120,
+      redFrom: 90, redTo: 100,
+      yellowFrom:75, yellowTo: 90,
+      minorTicks: 5,
+      min: 1
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('gauge'));
+
+    chart.draw(data, options);
+
+    setInterval(function() {
+      data.setValue(0, 25, 75);
+      chart.draw(data, options);
+    }, 13000);
   }
 });
