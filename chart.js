1fe2762c3cdd7b66fbb82d790776381e1104776f
gbeanblossom1 <106623684+gbeanblossom1@users.noreply.github.com>
2022-08-31 09:42:29 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -142,7 +142,7 @@ $(function runNonFileData() {
     ]);
 
     var options = {
-      width: 400, height: 120,
+      width: 800, height: 240,
       redFrom: 90, redTo: 100,
       yellowFrom:75, yellowTo: 90,
       minorTicks: 5,
